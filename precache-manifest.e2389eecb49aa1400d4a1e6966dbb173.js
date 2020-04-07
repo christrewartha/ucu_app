c418b226e4b4101cd447826b24529c338062ecd6
Chris Trewartha <christrewartha@hotmail.com>
2020-04-07 16:26:04 +0100

    Updates

---
+++ precache-manifest.e2389eecb49aa1400d4a1e6966dbb173.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf012f372b867022d2ab6c4373dc393",
+    "url": "/christrewartha/ucu_app/index.html"
+  },
+  {
+    "revision": "403375029246fa906d13",
+    "url": "/christrewartha/ucu_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dbbcb85967887bc1bf0d",
+    "url": "/christrewartha/ucu_app/static/js/2.456605d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/christrewartha/ucu_app/static/js/2.456605d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403375029246fa906d13",
+    "url": "/christrewartha/ucu_app/static/js/main.c4b9b9bf.chunk.js"
+  },
+  {
+    "revision": "dfcc7750d4c568b0e139",
+    "url": "/christrewartha/ucu_app/static/js/runtime-main.d440bdd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/christrewartha/ucu_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
